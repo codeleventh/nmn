7dfcad6ecebed27f7959e42909e51e1324ac79c2
codeleventh <eleventh@vechnost-eto-ne-srok.ru>
2019-08-04 22:58:34 +0500

    + error handler fix

--- src/main/java/ru/eleventh/nmn/ErrorHandler.java
+++ src/main/java/ru/eleventh/nmn/ErrorHandler.java
@@ -5,7 +5,6 @@ import java.util.Map;
 import org.hibernate.service.spi.ServiceException;
 import org.springframework.core.NestedRuntimeException;
 import org.springframework.dao.DataAccessException;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -20,17 +19,16 @@ public class ErrorHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({NoteNotFoundException.class})
     protected ResponseEntity<Object> handleNotFound(RuntimeException e, WebRequest request) {
-        return handleExceptionInternal(e, e.getMessage(),
-                                       null,
-                                       HttpStatus.NOT_FOUND, request);
+        Map<String, String> body = new HashMap<>();
+        body.put("error", e.getMessage());
+        return handleExceptionInternal(e, body, null, HttpStatus.NOT_FOUND, request);
     }
 
     @ExceptionHandler({EmptyQueryException.class})
     protected ResponseEntity<Object> handleEmptyQuery(RuntimeException e, WebRequest request) {
         Map<String, String> body = new HashMap<>();
         body.put("error", e.getMessage());
-
-        return handleExceptionInternal(e, body,null, HttpStatus.BAD_REQUEST, request);
+        return handleExceptionInternal(e, body, null, HttpStatus.BAD_REQUEST, request);
     }
 
     @ExceptionHandler({ServiceException.class, DataAccessException.class,
