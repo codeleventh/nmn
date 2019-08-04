752fd36d740196e3a57857a22d3e14414a7ab491
codeleventh <eleventh@vechnost-eto-ne-srok.ru>
2019-08-04 23:35:43 +0500

    - db init data

--- src/main/java/ru/eleventh/nmn/DatabaseLoader.java
+++ src/main/java/ru/eleventh/nmn/DatabaseLoader.java
@@ -16,22 +16,22 @@ public class DatabaseLoader implements CommandLineRunner {
 
     @Override
     public void run(String[] args) {
-        this.repository.save(new Note("high", "dry"));
-        this.repository.save(new Note("tall", "thin"));
-        this.repository.save(new Note("hot", "dog"));
-        this.repository.save(new Note("touch", "go"));
-        this.repository.save(new Note("husband", "wife"));
-        this.repository.save(new Note("trial", "error"));
-        this.repository.save(new Note("in", "out"));
-        this.repository.save(new Note("Jack", "White"));
-        this.repository.save(new Note("knife", "fork"));
-        this.repository.save(new Note("wait", "see"));
-        this.repository.save(new Note("ladies", "gentlemen"));
-        this.repository.save(new Note("war", "peace"));
-        this.repository.save(new Note("law", "order"));
-        this.repository.save(new Note("wine", "cheese"));
-        this.repository.save(new Note("бока", "жока"));
-        this.repository.save(new Note("пит", "буль"));
-        this.repository.save(new Note("список покупок", "огурцы, помидоры, мука"));
+//        this.repository.save(new Note("high", "dry"));
+//        this.repository.save(new Note("tall", "thin"));
+//        this.repository.save(new Note("hot", "dog"));
+//        this.repository.save(new Note("touch", "go"));
+//        this.repository.save(new Note("husband", "wife"));
+//        this.repository.save(new Note("trial", "error"));
+//        this.repository.save(new Note("in", "out"));
+//        this.repository.save(new Note("Jack", "White"));
+//        this.repository.save(new Note("knife", "fork"));
+//        this.repository.save(new Note("wait", "see"));
+//        this.repository.save(new Note("ladies", "gentlemen"));
+//        this.repository.save(new Note("war", "peace"));
+//        this.repository.save(new Note("law", "order"));
+//        this.repository.save(new Note("wine", "cheese"));
+//        this.repository.save(new Note("бока", "жока"));
+//        this.repository.save(new Note("пит", "буль"));
+//        this.repository.save(new Note("список покупок", "огурцы, помидоры, мука"));
     }
 }
